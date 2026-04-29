0beddfe6b817048ed93001413e9ae650f46447b4
clockhart <clockha2@gmu.edu>
2026-04-29 19:13:23 -0400

    adding stashed changes

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,4 +1,4 @@
-tag="0.1.1"
+tag="0.1.2"
 
 git checkout main
 git pull origin main
---
+++ tests/test_path.py
@@ -0,0 +1,11 @@
+
+from fpathlib import expand_fpath
+
+
+def test_expand_fpath():
+    # testcase1
+    paths = expand_fpath("testcase1/{letter:s}/{number:d}")
+    
+
+
+
