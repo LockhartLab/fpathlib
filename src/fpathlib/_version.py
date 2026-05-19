596782b51c763e84d03e9d354dbb1faf0268e0b0
clockhart <clockha2@gmu.edu>
2026-05-19 14:23:35 -0400

    reducing testcase1 size

--- src/fpathlib/_version.py
+++ src/fpathlib/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.7.dev0'
-__version_tuple__ = version_tuple = (0, 1, 7, 'dev0')
+__version__ = version = '0.1.9.dev0'
+__version_tuple__ = version_tuple = (0, 1, 9, 'dev0')
 
-__commit_id__ = commit_id = 'g73692f474'
+__commit_id__ = commit_id = 'g629a5a1b2'
