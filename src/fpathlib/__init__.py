6feb7fa63a51d884073f57cb92d456009c1a5a1d
clockhart <clockha2@gmu.edu>
2026-06-11 19:38:58 -0400

    making it easier to import ext.polars

--- src/fpathlib/__init__.py
+++ src/fpathlib/__init__.py
@@ -6,10 +6,13 @@ from fpathlib.path import (
     expand_fpath_decorator,
 )
 
+from fpathlib.ext import polars
+
 __all__ = [
     "Path",
     "FPath",
     "ExpandedFPath",
     "expand_fpath",
     "expand_fpath_decorator",
+    "polars",
 ]
