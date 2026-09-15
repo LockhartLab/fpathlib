dfa423812d79a696565547e5876cb6f2c3410d7d
clockhart <clockha2@gmu.edu>
2026-09-15 18:59:28 -0400

    tag 0.1.2

--- src/fpathlib/_version.py
+++ src/fpathlib/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.2.dev0'
-__version_tuple__ = version_tuple = (0, 1, 2, 'dev0')
+__version__ = version = '0.1.2.dev17'
+__version_tuple__ = version_tuple = (0, 1, 2, 'dev17')
 
-__commit_id__ = commit_id = 'g84a117f3e'
+__commit_id__ = commit_id = 'g183cc10c1'
